fbf8e1cb38329290df75a5bec52069999c2cf93a
Eddy Lei <eddylei070300@gmail.com>
2020-09-09 09:36:01 +0800

    Added categories and loading animation

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
-new Vue ({
+var LoadingBar = {
+  template:
+    '<div class="custom-progress" style="height: 10"><div class="custom-bar" style="height: 10px"></div></div>',
+};
+new Vue({
   el: "#app",
   data: {
-    activities: null
+    activities: null,
+    loading: true,
+    items: null,
+    areas: [
+      "Citizenship with Global Perspectives",
+      "Cultural Engagement",
+      "Healthy Living",
+      "Interpersonal Relation and Teamwork",
+      "Leadership and Service ",
+    ],
   },
-  mounted () {
+  components: {
+    "v-loading": LoadingBar,
+  },
+  mounted() {
     axios({
-      url: "https://wrapapi.com/use/ededdy/umwp/news/0.0.2",
-      method: 'post',
+      url: "https://wrapapi.com/use/ededdy/umwp/news/0.0.3",
+      method: "post",
       data: {
-        "wrapAPIKey": "1RpF8AqqGz5fdUrzikjKB8ZD9Qy2RJ0G"
+        // "wrapAPIKey": "1RpF8AqqGz5fdUrzikjKB8ZD9Qy2RJ0G"
+        wrapAPIKey: "1kL5Re2Vx3Evh5ITLS2oL0j26XObQjfE",
+      },
+    }).then((response) => {
+      this.activities = response.data.data.collection;
+      this.items = response.data.data.collection;
+      this.loading = false;
+    });
+  },
+  methods: {
+    updateItems(area, evt) {
+      document.querySelector(".active").classList.remove("active");
+      evt.target.classList.add("active");
+      if (area) {
+        this.items = this.activities.filter((item) => item.wpArea === area);
+      } else {
+        this.items = this.activities.filter(Boolean);
       }
-    }).then(response => {
-      this.activities = response.data.data.collection
-    })
-  }
-})
+    },
+  },
+});
